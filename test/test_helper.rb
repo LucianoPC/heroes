abba2f033983792f59c5c2363e8bedc97b9571d0
Marcos Ronaldo <marcos.rpj2@gmail.com>
2016-06-13 17:53:10 -0300

    hero scaffold

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,12 +3,8 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 # rails active suport
-module ActiveSupport
-  class TestCase
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
-    #
-    order.fixtures :all
-
-    # Add more helper methods to be used by all tests here...
-  end
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
+  fixtures :all
+  # Add more helper methods to be used by all tests here...
 end
